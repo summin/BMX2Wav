6dcb09f242af2633a76f189a96b2cfe9a83f2485
aleontjev <a.leontjev@pyrexx.com>
2020-08-29 23:55:00 +0200

    Initial Commit

---
+++ BMX2Wav/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BMX2Wav
+//
+//  Created by Aleksandr Leontjev on 29/08/2020.
+//  Copyright © 2020 Aleksandr Leontjev. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
