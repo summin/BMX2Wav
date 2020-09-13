16c54815aa934fb3bb6148b55e9f826b99e7e875
aleontjev <a.leontjev@pyrexx.com>
2020-09-13 08:34:23 +0200

    firstSavedSample

---
+++ BMX2Wav/Classes/ValueFinder.cpp
@@ -0,0 +1,9 @@
+//
+//  ValueFinder.cpp
+//  BMX2Wav
+//
+//  Created by Aleksandr Leontjev on 04/09/2020.
+//  Copyright © 2020 Aleksandr Leontjev. All rights reserved.
+//
+
+#include "ValueFinder.hpp"
---
+++ BMX2Wav/Classes/ValueFinder.hpp
@@ -0,0 +1,14 @@
+//
+//  ValueFinder.hpp
+//  BMX2Wav
+//
+//  Created by Aleksandr Leontjev on 04/09/2020.
+//  Copyright © 2020 Aleksandr Leontjev. All rights reserved.
+//
+
+#ifndef ValueFinder_hpp
+#define ValueFinder_hpp
+
+#include <stdio.h>
+
+#endif /* ValueFinder_hpp */
